f7f96b2728db17065264b6da397d65e96a1597d8
pachyyy <adri.emma05@gmail.com>
2025-05-24 17:37:34 +0700

    navigation bar component done

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,26 +3,28 @@ function NavBar() {
   return(
     <>
     <nav id="header" className="flex w-full flex-wrap align-items-center mx-auto justify-between p-2 fixed top-0 left-0 right-0 z-30">
-      {/* Logo  */}
-      <div className="flex justify-between items-center mx-auto">
-        <a href="#"><img src="./logo-navy.png" alt="logo" className="h-30 w-30" /></a>
-      </div>
+        <div className="flex justify-between items-center w-[98%] mx-auto">
+            {/* Logo  */}
+          <div className="">
+            <a href="#"><img src="./logo-navy.png" alt="logo" className="h-30 w-30" /></a>
+          </div>
 
-      {/* Nav Links */}
-      <div className="-ml-13">
-        <ul className="flex gap-[4vw] text-[#0069A8]">
-          <li className=" "><a href="#">Home</a></li>
-          <li className=" "><a href="#">Services & Price</a></li>
-          <li className=" "><a href="#">Outlet</a></li>
-          <li className=" "><a href="#">About Us</a></li>
-        </ul>
-      </div>
-      
-      {/* Order Button */}
-      <div>
-        <button className="bg-[#0069A8] text-white rounded-xl px-4 py-3 hover:bg-[#80acc7] cursor-pointer transition-ease-in-out"><a href="#">Order Now</a></button>
-      </div>
-    </nav>
+          {/* Nav Links */}
+          <div className="">
+            <ul className="flex gap-[4vw] text-[#0069A8]">
+              <li className="hover:text-white hover:bg-[#0069A8] transition-ease-in-out duration-300 cursor-pointer px-4 py-2 rounded-full"><a href="#">Home</a></li>
+              <li className="hover:text-white hover:bg-[#0069A8] transition-ease-in-out duration-300 cursor-pointer px-4 py-2 rounded-full"><a href="#">Services & Price</a></li>
+              <li className="hover:text-white hover:bg-[#0069A8] transition-ease-in-out duration-300 cursor-pointer px-4 py-2 rounded-full"><a href="#">Outlet</a></li>
+              <li className="hover:text-white hover:bg-[#0069A8] transition-ease-in-out duration-300 cursor-pointer px-4 py-2 rounded-full"><a href="#">About Us</a></li>
+            </ul>
+          </div>
+          
+          {/* Order Button */}
+          <div>
+            <button className="bg-[#0069A8] text-white rounded-xl px-4 py-3 hover:bg-[#80acc7] cursor-pointer transition-ease-in-out duration-300"><a href="#">Order Now</a></button>
+          </div>
+        </div>
+      </nav>
     </>
   );
 
